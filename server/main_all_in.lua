3ae574d3fd126bbb75632788388070d4f96d278d
zhangkaipeng <zhangkaipeng@dayugame.net>
2019-01-20 15:27:10 +0800

    test

--- server/main_all_in.lua
+++ server/main_all_in.lua
@@ -2,15 +2,15 @@ package.path = "./server/?.lua;" .. package.path
 local skynet=require "skynet"
 local datacenter=require "skynet.datacenter"
 local cluster=require "skynet.cluster"
+local const =require "server_const.const"
 require "common.function"
 
 --设置ENV
 setup_env()
 
 skynet.start(function()
-    local const =require "server_const.const"
-    skynet.error("Server Main Start!!!") 
     
+    skynet.error("Server Main Start!!!") 
     skynet.error(tostring(const.ITEM_TAG));
 
 

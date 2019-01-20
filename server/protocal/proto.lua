bdd10b104011d17c56f88ea5f9cd24618471965a
zhangkaipeng <zhangkaipeng@dayugame.net>
2019-01-20 16:31:44 +0800

    test

--- server/protocal/proto.lua
+++ server/protocal/proto.lua
@@ -2,16 +2,16 @@ local sprotoparser = require "sprotoparser"
 
 local proto = {}
 
-local c2s.sproto = nil
+local c2s_proto = nil
 local  file = io.open("server/protocal/c2s.sproto")
 c2s_proto=file:read("*a")
 file:close()
-proto.c2s=sprotoparser.parse(c2s.sproto)
+proto.c2s=sprotoparser.parse(c2s_proto)
 
-local s2c.sproto = nil
+local s2c_proto = nil
 file = io.open("server/protocal/s2c.sproto")
-s2c.sproto=file:read("*a")
+s2c_proto=file:read("*a")
 file:close()
-proto.s2c=sprotoparser.parse(s2c.sproto)
+proto.s2c=sprotoparser.parse(s2c_proto)
 
 return proto
